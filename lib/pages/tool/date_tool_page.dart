638f2dbf74c1c1fa979c51156a3a94572973c5ac
daichangxia <changxia.dai@theone.art>
2023-09-12 20:28:22 +0800

    新功能点: 时间工具页面

--- lib/pages/tool/date_tool_page.dart
+++ lib/pages/tool/date_tool_page.dart
@@ -16,6 +16,7 @@ class _DateToolPage extends State<DateToolPage> {
   //时间格式，1：毫秒。2：秒
   int dateTypeSelect = 1;
   late TextEditingController _controller;
+  late TextEditingController dateController;
 
   @override
   void initState() {
@@ -31,17 +32,19 @@ class _DateToolPage extends State<DateToolPage> {
           title: const Text("时间工具"),
         ),
         body: Center(
-          child: Container(
+          child: SizedBox(
             width: 0.5.sw,
             height: 0.5.sh,
             // color: Colors.greenAccent,
             child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
+
                 children: [
                   SelectableText(
                     "当前时间戳（毫秒）是： $currentTimeStamp",
                     style: const TextStyle(color: Colors.black, fontSize: 20),
                   ),
+                  Container(height: 10,),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
@@ -65,25 +68,85 @@ class _DateToolPage extends State<DateToolPage> {
                       ),
                     ],
                   ),
+                  Container(height: 10,),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       const Text("时间戳："),
-                      Container(
-                          width: 230,
+                      SizedBox(
+                          width: 220,
                           child: TextField(
                               controller: _controller,
                               decoration: InputDecoration(
                                 suffixIcon: IconButton(
-                                  icon: Icon(Icons.close),
-                                  onPressed: () {},
+                                  icon: const Icon(Icons.close),
+                                  onPressed: () {
+                                    _controller.clear();
+                                  },
                                 ),
-                                border: OutlineInputBorder(
+                                border: const OutlineInputBorder(
                                   borderRadius: BorderRadius.all(
                                     Radius.circular(10),
                                   ),
                                 ),
                               ))),
+                      SizedBox(width: 10,),
+                      DropdownButton(
+                        focusColor: Colors.transparent,
+                        value: dateTypeSelect,
+                        items: <DropdownMenuItem<int>>[
+                          DropdownMenuItem(
+                            value: 1,
+                            child: Text(
+                              "毫秒",
+                              style: TextStyle(
+                                  color: dateTypeSelect == 1
+                                      ? Colors.greenAccent
+                                      : Colors.grey),
+                            ),
+                          ),
+                          DropdownMenuItem(
+                            value: 2,
+                            child: Text(
+                              "秒",
+                              style: TextStyle(
+                                  color: dateTypeSelect == 2
+                                      ? Colors.greenAccent
+                                      : Colors.grey),
+                            ),
+                          ),
+                        ],
+                        onChanged: (int? value) {
+                          setState(() {
+                            dateTypeSelect = value!;
+                          });
+                        },
+                      )
+                    ],
+                  ),
+                  Container(height: 10,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text("时间    ："),
+                      SizedBox(
+                          width: 220,
+                          child: TextField(
+                              controller: _controller,
+                              decoration: InputDecoration(
+                                suffixIcon: IconButton(
+                                  icon: const Icon(Icons.close),
+                                  onPressed: () {
+                                    _controller.clear();
+                                  },
+                                ),
+                                border: const OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(10),
+                                  ),
+                                ),
+                              ))),
+                      SizedBox(width: 10,),
                       DropdownButton(
                         focusColor: Colors.transparent,
                         value: dateTypeSelect,
